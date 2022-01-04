7e66de279450562b0707bf010ff0cafb44449e45
Sabau Valeriu Ilie <valeriusabau29@gmail.com>
2022-01-04 23:38:59 +0200

    select the time frame for the chart.

--- EnergyManagementApp/app/src/main/java/com/example/energymanagementapp/BatteryManagmentActivity.kt
+++ EnergyManagementApp/app/src/main/java/com/example/energymanagementapp/BatteryManagmentActivity.kt
@@ -1,7 +1,11 @@
 package com.example.energymanagementapp
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.energymanagementapp.models.BatteryPowerDataList
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.AxisBase
@@ -10,6 +14,7 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 import com.prolificinteractive.materialcalendarview.OnRangeSelectedListener
 import java.text.SimpleDateFormat
@@ -19,16 +24,17 @@ import kotlin.collections.ArrayList
 class BatteryManagmentActivity : AppCompatActivity() {
     private lateinit var lineChart: LineChart
     private lateinit var materialCalendar: MaterialCalendarView
-    data class Score(
-        val date:Date,
-        val value: Int,
-    )
-    private var scoreList = ArrayList<Score>()
+    private lateinit var selectButton:Button
+
+    private var calendarDays=ArrayList<CalendarDay>()
+
+    private var batteryPowerList = BatteryPowerDataList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_battery_managment)
         lineChart = findViewById(R.id.chart)
+        selectButton = findViewById(R.id.selectDateButton)
 
         materialCalendar = findViewById<MaterialCalendarView>(R.id.calendarView)
 
@@ -36,8 +42,20 @@ class BatteryManagmentActivity : AppCompatActivity() {
         println( "set event")
         materialCalendar.setOnRangeSelectedListener(
             OnRangeSelectedListener(fun(view,dates){
-            println(dates)
+
+                materialCalendar.visibility = View.GONE
+                selectButton.visibility=View.VISIBLE
+                calendarDays = dates as ArrayList<CalendarDay>
+                setDataToLineChart()
+                lineChart.notifyDataSetChanged()
+                lineChart.invalidate()
+                println(dates)
         }))
+
+        selectButton.setOnClickListener {
+            materialCalendar.visibility = View.VISIBLE
+            selectButton.visibility=View.GONE
+        }
         initLineChart()
 
 
@@ -78,9 +96,9 @@ class BatteryManagmentActivity : AppCompatActivity() {
 
         override fun getAxisLabel(value: Float, axis: AxisBase?): String {
             val index = value.toInt()
-            return if (index < scoreList.size) {
+            return if (index < batteryPowerList.data.size) {
                 val sdf = SimpleDateFormat("dd MMM hh")
-                return sdf.format( scoreList[index].date);
+                return sdf.format( batteryPowerList.data[index].date);
               // .toString()
             } else {
                 ""
@@ -92,12 +110,33 @@ class BatteryManagmentActivity : AppCompatActivity() {
         //now draw bar chart with dynamic data
         val entries: ArrayList<Entry> = ArrayList()
 
-        scoreList = getScoreList()
+       // batteryPowerList = getBatteryPowerList()
 
         //you can replace this data object with  your custom object
-        for (i in scoreList.indices) {
-            val score = scoreList[i]
-            entries.add(Entry(i.toFloat(), score.value.toFloat()))
+        // check if in time interval
+        println("set data charts !!!")
+        if(calendarDays.size == 0){
+            for (i in batteryPowerList.data.indices) {
+                val score = batteryPowerList.data[i]
+                entries.add(Entry(i.toFloat(), score.value.toFloat()))
+            }
+        }else {
+            for (i in batteryPowerList.data.indices) {
+                val score = batteryPowerList.data[i]
+                println(calendarDays[0].date )
+
+                println(score.date  )
+                println(calendarDays.last().date)
+
+                println("first compare")
+                println(calendarDays[0].date.before(score.date ))
+                println("last compare")
+                println( score.date.before(calendarDays.last().date))
+                if(calendarDays[0].date.before(score.date )&&
+                    score.date.before(calendarDays.last().date) ) {
+                    entries.add(Entry(i.toFloat(), score.value.toFloat()))
+                }
+            }
         }
 
         val lineDataSet = LineDataSet(entries, "")
@@ -111,15 +150,7 @@ class BatteryManagmentActivity : AppCompatActivity() {
 
     // simulate api call
     // we are initialising it directly
-    private fun getScoreList(): ArrayList<Score> {
-        scoreList.add(Score(Date(2022,1,4,1,0), 56))
-        scoreList.add(Score(Date(2022,1,4,2,0), 75))
-        scoreList.add(Score(Date(2022,1,4,3,0), 85))
-        scoreList.add(Score(Date(2022,1,4,4,0), 45))
-        scoreList.add(Score(Date(2022,1,4,5,0), 63))
 
-        return scoreList
-    }
 
 
 
---
+++ EnergyManagementApp/app/src/main/java/com/example/energymanagementapp/models/BatteryPowerDataList.kt
@@ -0,0 +1,19 @@
+package com.example.energymanagementapp.models
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+class BatteryPowerDataList() {
+
+    var data = ArrayList<BatteryPowerData>()
+
+    init {
+        data.add(BatteryPowerData(Date(2022-1900, 0, 29, 1, 0), 56))
+        data.add(BatteryPowerData(Date(2022-1900, 0, 30, 2, 0), 75))
+        data.add(BatteryPowerData(Date(2022-1900, 1, 4, 3, 0), 85))
+        data.add(BatteryPowerData(Date(2022-1900, 1, 4, 4, 0), 45))
+        data.add(BatteryPowerData(Date(2022-1900, 1, 4, 5, 0), 63))
+    }
+
+
+}
