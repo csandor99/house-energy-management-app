d84121f322f9cccd8b8d835cffe68c4a38e95e3d
Sabau Valeriu Ilie <valeriusabau29@gmail.com>
2022-01-05 11:52:44 +0200

    spinner added for the e charging status.

--- EnergyManagementApp/app/src/main/java/com/example/energymanagementapp/ECarChargingActivity.kt
+++ EnergyManagementApp/app/src/main/java/com/example/energymanagementapp/ECarChargingActivity.kt
@@ -2,10 +2,24 @@ package com.example.energymanagementapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+
+
 
 class ECarChargingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_ecar_charging)
+
+        setContentView(R.layout.activity_ecar_charging)
+
+        val dropdown = findViewById<Spinner>(R.id.eCarSpinner)
+
+        val items = arrayOf("Photovoltaic mode", "Fast grid mode", "Smart mode")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+
+        dropdown.adapter = adapter
     }
 }
