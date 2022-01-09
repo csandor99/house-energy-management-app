12140ceefbec2dabbb18b0f80dca7032d9ab6f44
Sabau Valeriu Ilie <valeriusabau29@gmail.com>
2022-01-09 23:36:14 +0200

    slope adjusment activity done.

--- EnergyManagementApp/app/src/main/java/com/example/energymanagementapp/SlopeAdjustmentActivity.kt
+++ EnergyManagementApp/app/src/main/java/com/example/energymanagementapp/SlopeAdjustmentActivity.kt
@@ -1,13 +1,23 @@
 package com.example.energymanagementapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
+import android.widget.ToggleButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.energymanagementapp.adapters.PanelCustomAdapter
 import com.example.energymanagementapp.models.PanelViewModel
+import com.google.android.material.slider.Slider
 
 class SlopeAdjustmentActivity : AppCompatActivity() {
+
+    private val data = ArrayList<PanelViewModel>()
+    private lateinit var  adapter:PanelCustomAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slope_adjustment)
@@ -17,18 +27,55 @@ class SlopeAdjustmentActivity : AppCompatActivity() {
         recyclerview.layoutManager = LinearLayoutManager(this)
 
         // ArrayList of class ItemsViewModel
-        val data = ArrayList<PanelViewModel>()
 
         // This loop will create 20 Views containing
         // the image with the count of view
         for (i in 1..20) {
-            data.add(PanelViewModel( "Item " + i))
+            data.add(PanelViewModel( "Panel " + i,0f,false,false))
         }
 
         // This will pass the ArrayList to our Adapter
-        val adapter = PanelCustomAdapter(data)
-
+        adapter = PanelCustomAdapter(data)
         // Setting the Adapter with the recyclerview
+
+
+        adapter.listener= {x -> showDialog("Open Dialog",x)
+
+        };
         recyclerview.adapter = adapter
+
+
+
+    }
+
+    private fun showDialog(title: String,position: Int) {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.panel_dialog_view)
+        //val body = dialog.findViewById(R.id.body) as TextView
+        //body.text = title
+        val yesBtn = dialog.findViewById(R.id.yesBtn) as Button
+        val noBtn = dialog.findViewById(R.id.noBtn) as TextView
+        val slopeSlider = dialog.findViewById(R.id.slopeSlider) as Slider
+        val toggleButton = dialog.findViewById(R.id.toggleButton) as ToggleButton
+
+        slopeSlider.value=data[position].slope;
+        toggleButton.isChecked = data[position].auto;
+
+        slopeSlider.isEnabled = !toggleButton.isChecked
+
+        toggleButton.setOnClickListener{
+            slopeSlider.isEnabled = !toggleButton.isChecked
+        }
+        yesBtn.setOnClickListener {
+
+            data[position].slope = slopeSlider.value
+            data[position].auto= toggleButton.isChecked
+            adapter.notifyItemChanged(position)
+            dialog.dismiss()
+        }
+        noBtn.setOnClickListener { dialog.dismiss() }
+        dialog.show()
     }
 }
--- EnergyManagementApp/app/src/main/java/com/example/energymanagementapp/adapters/PanelCustomAdapter.kt
+++ EnergyManagementApp/app/src/main/java/com/example/energymanagementapp/adapters/PanelCustomAdapter.kt
@@ -1,15 +1,25 @@
 package com.example.energymanagementapp.adapters
 
+import android.graphics.Color
+import android.opengl.Visibility
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.energymanagementapp.R
 import com.example.energymanagementapp.models.PanelViewModel
 
-class PanelCustomAdapter(private val mList: List<PanelViewModel>) : RecyclerView.Adapter<PanelCustomAdapter.ViewHolder>() {
+class PanelCustomAdapter(private val mList: List<PanelViewModel>
 
+) : RecyclerView.Adapter<PanelCustomAdapter.ViewHolder>() {
+
+    var listener: ((Int) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // inflates the card_view_design view
@@ -23,15 +33,28 @@ class PanelCustomAdapter(private val mList: List<PanelViewModel>) : RecyclerView
     // binds the list items to a view
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        val ItemsViewModel = mList[position]
+        val itemViewModel = mList[position]
 
         // sets the image to the imageview from our itemHolder class
         // holder.imageView.setImageResource(ItemsViewModel.image)
-
         // sets the text to the textview from our itemHolder class
-        holder.textView.text = ItemsViewModel.text
 
+        if(itemViewModel.auto) {
+            holder.autoIconView.visibility = VISIBLE
+        }else {
+            holder.autoIconView.visibility = INVISIBLE
+        }
+
+        holder.textView.text = (itemViewModel.text  + " slope: " + String.format("%.2f", itemViewModel.slope)  )
+        if(itemViewModel.selected){
+
+            holder.cardView.setCardBackgroundColor(Color.CYAN)
+        }
+
+        holder.itemView.setOnClickListener { listener?.let { it1 -> it1(position) } }
     }
+
+
 
     // return the number of the items in the list
     override fun getItemCount(): Int {
@@ -41,5 +64,11 @@ class PanelCustomAdapter(private val mList: List<PanelViewModel>) : RecyclerView
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         //val imageView: ImageView = itemView.findViewById(R.id.imageview)
         val textView: TextView = itemView.findViewById(R.id.textView)
+        val cardView: CardView = itemView.findViewById(R.id.panelCardView)
+        val autoIconView:ImageView = itemView.findViewById(R.id.autoIcon)
+
+
     }
 }
+
+
