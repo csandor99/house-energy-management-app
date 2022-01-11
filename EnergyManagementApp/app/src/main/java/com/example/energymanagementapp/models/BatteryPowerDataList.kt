cda0a33fc192cfa08b3e064c0d0acf89ed22de18
Sabau Valeriu Ilie <valeriusabau29@gmail.com>
2022-01-11 20:08:16 +0200

    Add menu.

--- EnergyManagementApp/app/src/main/java/com/example/energymanagementapp/models/BatteryPowerDataList.kt
+++ EnergyManagementApp/app/src/main/java/com/example/energymanagementapp/models/BatteryPowerDataList.kt
@@ -8,6 +8,9 @@ class BatteryPowerDataList() {
     var data = ArrayList<BatteryPowerData>()
 
     init {
+        data.add(BatteryPowerData(Date(2022-1900, 0, 1, 5, 0), 75))
+        data.add(BatteryPowerData(Date(2022-1900, 0, 4, 5, 0), 75))
+        data.add(BatteryPowerData(Date(2022-1900, 0, 5, 5, 0), 75))
         data.add(BatteryPowerData(Date(2022-1900, 0, 29, 1, 0), 56))
         data.add(BatteryPowerData(Date(2022-1900, 0, 30, 2, 0), 75))
         data.add(BatteryPowerData(Date(2022-1900, 1, 4, 3, 0), 85))
